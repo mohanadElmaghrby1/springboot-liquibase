4fd1558acc509c383acb5ad4973516d05057b2bf
Mohannad <mohanad2021996@gmail.com>
2020-05-07 11:27:49 +0200

    Initial commit

---
+++ src/test/java/com/mohannad/springbootliquibase/SpringbootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohannad.springbootliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
